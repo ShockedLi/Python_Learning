daf31f95efdd46a2ed6314c3f89479dd465b9fec
shockedli <979036525@qq.com>
2022-10-06 17:51:21 +0800

    123

--- Code/helloworld.py
+++ Code/helloworld.py
@@ -19,12 +19,61 @@
 
 # print(lookupMaxMin(l))
 
-def fab(max):
-    n, a, b = 0, 0, 1
-    while n < max:
-        yield b
-        a, b = b, a + b
-        n = n+ 1
-#执行
-for n in fab(5):
-    print(n)
+# def fab(max):
+#     n, a, b = 0, 0, 1
+#     while n < max:
+#         yield b
+#         a, b = b, a + b
+#         n = n+ 1
+# #执行
+# for n in fab(5):
+#     print(n)
+
+# def f1():
+#     print(1/0)
+
+# def f2():
+#     f1()
+
+# def f3():
+#     f2()
+
+# f3()
+ 
+from re import A
+
+
+# #a = 1
+# def outer():
+#     a = 10 
+#     def inner():
+#         nonlocal a
+#         #global a
+#         a += 1
+#         print(a)
+#     return inner
+
+# outer_result = outer()
+# outer_result()
+# outer_result()
+
+
+
+# print(id)
+
+def log(func):
+    def wrapper(*args, **kwargs):
+        print('调用的函数名为%s' % func.__name__)
+        print('args=%s,kwargs=%s' % (args,kwargs))
+        return func(*args,**kwargs)
+    return wrapper
+ 
+
+
+@log
+def add(a,b):
+    return a+b
+
+
+result = add(1,2)
+print(result)
